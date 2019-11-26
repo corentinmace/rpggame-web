abf1da9e8d0d22184fda0441a77edab15ecfe564
Mace <corentin.mace@ynov.com>
2019-11-26 06:19:53 +0100

    v0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 let audio = document.getElementById("audio");
+let mainMenu = document.getElementById("main-menu");
+let settingsMenu = document.getElementById("settings");
+
 
 function muteVolume() {
   audio.pause();
@@ -6,4 +9,24 @@ function muteVolume() {
 
 function playMusic() {
   audio.play();
+}
+
+function showMainMenu() {
+  mainMenu.style.display = 'flex';
+}
+
+function hideMainMenu() {
+  mainMenu.style.display = 'none';
+}
+
+function showSettings() {
+  settingsMenu.style.display = 'block';
+
+  hideMainMenu();
+}
+
+function hideSettingsMenu() {
+  settingsMenu.style.display = 'none';
+
+  showMainMenu();
 }
